4024cdb7a50e329e2c8899d2c698d82787ced933
Jose Castro <drakonc@users.noreply.github.com>
2019-05-23 16:24:33 -0500

    Adecuaciones del Formulario

--- src/assets/js/fontawesome.js
+++ src/assets/js/fontawesome.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faTicketAlt, faDatabase, faUser, faAddressCard, faPhoneSquare, faEnvelope, faCubes, faFileAlt }
-from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faTicketAlt, faDatabase, faUser, faAddressCard, faPhoneSquare, faEnvelope, faCubes, faFileAlt, faExclamation, faExclamationTriangle, faCogs }
+    from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCoffee)
 library.add(faTicketAlt)
@@ -11,6 +11,9 @@ library.add(faPhoneSquare)
 library.add(faEnvelope)
 library.add(faCubes)
 library.add(faFileAlt)
+library.add(faExclamation)
+library.add(faExclamationTriangle)
+library.add(faCogs)
 
 
 export default library;
--- src/components/ticket/Ticket.vue
+++ src/components/ticket/Ticket.vue
@@ -48,17 +48,41 @@
                                                 <div class="card-header bg-primary text-white font-weight-bold"><font-awesome-icon icon="cubes" /> Información Geneal</div>
                                                 <div class="card-body">
                                                     <div class="form-group input-group mb-3">
-                                                        <input type="text" name="area" class="form-control" placeholder="Area Solicitante" aria-describedby="iarea">
+                                                        <select class="custom-select form-control" name="area" aria-describedby="itsolici">
+                                                          <option selected>Tipo de Solicitud</option>
+                                                          <option value="1">Incidencia</option>
+                                                          <option value="2">Solicitud</option>
+                                                        </select>
                                                         <div class="input-group-prepend">
-                                                            <span class="input-group-text" id="iarea"><font-awesome-icon icon="user" /></span>
+                                                            <span class="input-group-text" id="itsolici"><font-awesome-icon icon="exclamation" /></span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group input-group mb-3">
+                                                        <select class="custom-select form-control" name="solicitud" aria-describedby="isolici">
+                                                          <option selected>Problema</option>
+                                                          <option value="1">Incidencia</option>
+                                                          <option value="2">Solicitud</option>
+                                                        </select>
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="isolici"><font-awesome-icon icon="exclamation-triangle" /></span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group input-group mb-3">
+                                                        <select class="custom-select form-control" name="tecnico" aria-describedby="itecnico">
+                                                          <option selected>Tecnico de Sistemas</option>
+                                                          <option value="1">Incidencia</option>
+                                                          <option value="2">Solicitud</option>
+                                                        </select>
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text" id="itecnico"><font-awesome-icon icon="cogs" /></span>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-md-12 pt-3">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-6 pt-3">
                                             <div class="card border border-success">
                                                 <div class="card-header bg-success text-white font-weight-bold"><font-awesome-icon icon="file-alt" /> Descripción</div>
                                                 <div class="card-body">
@@ -66,12 +90,10 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="row pt-3">
-                                    <div class="col-md-8 offset-md-2">
-                                        <button class="btn btn-primary btn-block text-white"><i class="fas fa-ticket-alt"></i>
-                                            Crear Ticket</button>
+                                        <div class="col-md-6 pt-3">
+                                            <button class="btn btn-primary btn-lg btn-block text-white"><i class="fas fa-ticket-alt"></i>
+                                                Crear Ticket</button>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
@@ -85,7 +107,7 @@
 
 <script>
     import navTicket from '@/components/ticket/navTicket.vue'
-    
+
     export default {
         name:'tick',
         components: {
