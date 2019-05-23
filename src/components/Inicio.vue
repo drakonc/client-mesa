b14ee31cca8fbe0994659a2647c6318add3c5056
Jose Castro <drakonc@gmail.com>
2019-05-22 20:45:23 -0500

    Arregos Cliente

---
+++ src/components/Inicio.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="ini">
+        <div class="container-fluid pt-3">
+            <div class="row align-items-start">
+                <div class="col-md-8 offset-md-2">
+                    <div class="card border border-primary">
+                        <div class="card-header text-center bg-primary">
+                            <h2 class="card-title text-white text-uppercase">Bienvenido a la Mesa de Servicio</h2>
+                        </div>
+                        <div class="card-body">
+                            <img src="@/assets/logo.jpg" height="450" class="card-img-top" alt="...">
+                            <div class="pt-3">
+                                <a href="/ticket" class="btn btn-primary btn-block text-white"><i class="fas fa-ticket-alt"></i> Crear Ticket</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ini'
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/views/HomeTicket.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="homeTicket">
+    <Ticket/> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Ticket from '@/components/Ticket.vue'
+
+export default {
+  name: 'homeTicket',
+  components: {
+    Ticket
+  }
+}
+</script>
